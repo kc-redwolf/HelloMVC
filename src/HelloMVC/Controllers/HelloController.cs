e16d915ad01f2ff2a76399a678259f700b7491b5
Heather Cunningham <kcredwolf@gmail.com>
2017-03-13 08:23:19 -0500

    Changess made following Routes video

--- src/HelloMVC/Controllers/HelloController.cs
+++ src/HelloMVC/Controllers/HelloController.cs
@@ -7,20 +7,32 @@ namespace HelloMVC.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/Hello/Display'>" +
+            string html = "<form method='post'>" +
                 "<input type='text' name='name' />" +
                 "<input type='submit' value='Greet Me!' />" +
                 "</form>";
 
-            return Content(string.Format("<h1>Hello {0}!</h1>", name), "text/html");
+            return Redirect("/Hello/Goodbye");
         }
+        // /Hello
+        [Route("/Hello")]
+        [HttpPost]
+
         public IActionResult Display(string name = "World")
         {
             return Content(string.Format("<h1>Hello {0}!</h1>", name), "text/html");
         }
         
+        // Handle requests to /Hello/NAME (URL segment)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}!</h1>", name), "text/html");
+        }
+
         public IActionResult Goodbye()
         {
             return Content("Goodbye");
