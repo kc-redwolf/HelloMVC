64ebc5b2615627bd14d31fefd060af4853b02324
Heather Cunningham <kcredwolf@gmail.com>
2017-03-13 07:36:42 -0500

    Initial commit

---
+++ src/HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/Hello/Display'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+            return Content(string.Format("<h1>Hello {0}!</h1>", name), "text/html");
+        }
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}!</h1>", name), "text/html");
+        }
+        
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+    }
+
+}
